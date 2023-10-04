0e4eef90eb156663572f7e08f62ff139d0ba2fa6
Tair2033 <tairenikeev@gmail.com>
2023-10-04 10:10:21 +0400

    test

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  data() {
+    return {
+      
+    }
+  }
+})
+</script>
