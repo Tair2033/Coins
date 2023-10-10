4c2208bab2652b27b00a5f5867570af805db371c
Tair2033 <tairenikeev@gmail.com>
2023-10-10 11:36:07 +0400

    updated coins page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import CoinsView from '../views/CoinsView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/',
+    name: 'coins',
+    component: CoinsView
   }
 ];
 
---
+++ src/views/CoinsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Coins
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'CoinsView'
+})
+</script>
+
+<style lang="scss" scoped></style>
